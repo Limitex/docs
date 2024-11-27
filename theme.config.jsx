dadeac08e8112a2aa7dfb9db18454e9a53cd6261
Limitex <76650151+Limitex@users.noreply.github.com>
2024-11-28 07:56:49 +0900

    Update site information

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,29 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>Limitex Documents</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Limitex Docs" />
+      <meta property="og:description" content="Limitex Documents" />
+    </>
+  ),
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://github.com/Limitex/docs",
   },
-  // ... other theme options
+  docsRepositoryBase: "https://github.com/Limitex/docs/blob/main",
+  feedback: {
+    content: "Feedback →",
+    useLink: () => "https://github.com/Limitex/docs/issues/new",
+  },
+  footer: {
+    content: (
+      <span>
+        © {new Date().getFullYear()}{" "}
+        <a href="https://github.com/Limitex" target="_blank">
+          Limitex
+        </a>
+        .
+      </span>
+    ),
+  },
 };
