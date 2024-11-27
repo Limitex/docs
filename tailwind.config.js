22a3e655f67c1f427e5c18eb45c0bef6c257e9ba
Limitex <76650151+Limitex@users.noreply.github.com>
2024-11-28 07:46:48 +0900

    Change src directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,jsx,ts,tsx,md,mdx}",
-    "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
-    "./components/**/*.{js,jsx,ts,tsx,md,mdx}",
+    "./src/app/**/*.{js,jsx,ts,tsx,md,mdx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx,md,mdx}",
+    "./src/components/**/*.{js,jsx,ts,tsx,md,mdx}",
   ],
   theme: {
     extend: {},
