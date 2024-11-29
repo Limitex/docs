efdfc91042e0992146d14b30badb91414c63e5dd
Limitex <76650151+Limitex@users.noreply.github.com>
2024-11-29 20:59:05 +0900

    feat: add mono-ui pages

--- src/pages/vrc/monoui/_meta.ts
+++ src/pages/vrc/monoui/_meta.ts
@@ -1,3 +1,29 @@
 export default {
+  "--- Getting Started": {
+    type: "separator",
+    title: "Getting Started",
+  },
   index: "Introduction",
+  installation: "Installation",
+  "--- Examples": {
+    type: "separator",
+    title: "Examples",
+  },
+  uisample: "UI Sample",
+  "--- Components": {
+    type: "separator",
+    title: "Components",
+  },
+  button: "Button",
+  dropdown: "Dropdown",
+  inputfield: "InputField",
+  "--- More": {
+    type: "separator",
+    title: "More",
+  },
+  github_link: {
+    title: "Mono UI",
+    href: "https://github.com/Limitex/mono-ui",
+    newWindow: true,
+  },
 };
