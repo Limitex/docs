f29621c03fd1593460fe4f66b579cbad93f5feb5
Limitex <76650151+Limitex@users.noreply.github.com>
2024-12-18 20:49:22 +0900

    feat: add toggle group to toggle page

--- src/components/unity-hierarchy/toggle.tsx
+++ src/components/unity-hierarchy/toggle.tsx
@@ -26,9 +26,34 @@ const ToggleDefault = (props: Partial<TreeItemProps> = {}): ComponentResult => {
   };
 };
 
+const ToggleGroup = (props: Partial<TreeItemProps> = {}): ComponentResult => {
+  const displayName = props.name ?? "Toggle Group";
+  const toggle1 = ToggleDefault({ name: "Toggle (0)" });
+  const toggle2 = ToggleDefault({ name: "Toggle (1)" });
+  const toggle3 = ToggleDefault({ name: "Toggle (2)" });
+
+  return {
+    data: {
+      title: "Toggle Group",
+      img: "https://via.placeholder.com/500x400",
+      href: "/vrc/monoui/toggle",
+      assetsPath: "/Packages/Mono UI/Runtime/Assets/Prefab/Components/Toggle/Toggle Group.prefab",
+      dependencies: [toggle1.data, toggle2.data, toggle3.data],
+    },
+    content: (
+      <Tree.Item name={displayName} type={Package} {...props}>
+        {toggle1.content}
+        {toggle2.content}
+        {toggle3.content}
+      </Tree.Item>
+    ),
+  };
+};
+
 const Toggle: Record<string, (props?: Partial<TreeItemProps>) => ComponentResult> = {
   Default: ToggleDefault,
   Outline: (props) => ToggleDefault({ name: "Toggle Outline", ...props }),
+  Group: ToggleGroup,
 };
 
-export default Toggle; 
+export default Toggle;
