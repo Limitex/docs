7c56d3b2be32921dcfa05952d78b6dc828c9376c
Limitex <76650151+Limitex@users.noreply.github.com>
2025-01-03 00:17:41 +0900

    fix: sidebar page hierarchie

--- src/components/unity-hierarchy/sidebar.tsx
+++ src/components/unity-hierarchy/sidebar.tsx
@@ -8,9 +8,10 @@ import Button from "./button";
 const createScrollView = (
   name: string,
   children?: React.ReactNode,
-  childDependency?: ComponentResult
+  childDependency?: ComponentResult,
+  props?: Partial<TreeItemProps>
 ): ComponentResult => {
-  const scrollView = ScrollView.Default({ name });
+  const scrollView = ScrollView.Default({ name, ...props });
   return {
     ...scrollView,
     data: {
@@ -18,7 +19,7 @@ const createScrollView = (
       dependencies: childDependency ? [childDependency.data] : [],
     },
     content: (
-      <Tree.Item name={name} type={Package}>
+      <Tree.Item name={name} type={Package} {...props}>
         <Tree.Item name="Viewport" type={Box}>
           <Tree.Item name="Content" type={Box}>
             {children}
@@ -40,19 +41,53 @@ const SidebarDefault = (
   const displayName = props.name ?? "Sidebar";
   const leftHeaderText = Text.H2({ name: "Text h2 (TMP)" });
   const rightHeaderText = Text.H3({ name: "Text h3 (TMP)" });
-  const buttonText = Text.H5({ name: "Text h5 (TMP)" });
-  const button = Button.Default({ name: "Button (Page 1)" });
-  button.data.dependencies = [buttonText.data];
 
-  const leftScrollView = createScrollView(
-    "Scroll View",
-    button.content,
-    button
+  const buttonText1 = Text.H5({ name: "Text h5 (TMP)" });
+  const buttonText2 = Text.H5({ name: "Text h5 (TMP)" });
+  const buttonText3 = Text.H5({ name: "Text h5 (TMP)" });
+
+  const button1 = Button.Default({ name: "Button (Page 1)" });
+  const button2 = Button.Default({ name: "Button (Page 2)" });
+  const button3 = Button.Default({ name: "Button (Page 3)" });
+
+  button1.data.dependencies = [buttonText1.data];
+  button2.data.dependencies = [buttonText2.data];
+  button3.data.dependencies = [buttonText3.data];
+
+  const leftScrollView1 = createScrollView(
+    "Scroll View (Page 1)",
+    button1.content,
+    button1
   );
-  const rightScrollView = createScrollView(
+  const leftScrollView2 = createScrollView(
+    "Scroll View (Page 2)",
+    button2.content,
+    button2,
+    { defaultExpanded: false }
+  );
+  const leftScrollView3 = createScrollView(
+    "Scroll View (Page 3)",
+    button3.content,
+    button3,
+    { defaultExpanded: false }
+  );
+
+  const rightScrollView1 = createScrollView(
     "Scroll View (Page 1)",
     undefined,
     undefined
+  );
+  const rightScrollView2 = createScrollView(
+    "Scroll View (Page 2)",
+    undefined,
+    undefined,
+    { defaultExpanded: false }
+  );
+  const rightScrollView3 = createScrollView(
+    "Scroll View (Page 3)",
+    undefined,
+    undefined,
+    { defaultExpanded: false }
   );
 
   return {
@@ -65,9 +100,13 @@ const SidebarDefault = (
       contextMenuPath: "GameObject/Mono UI/Layout/Sidebar",
       dependencies: [
         leftHeaderText.data,
-        leftScrollView.data,
+        leftScrollView1.data,
+        leftScrollView2.data,
+        leftScrollView3.data,
         rightHeaderText.data,
-        rightScrollView.data,
+        rightScrollView1.data,
+        rightScrollView2.data,
+        rightScrollView3.data,
       ],
     },
     content: (
@@ -77,13 +116,17 @@ const SidebarDefault = (
           <Tree.Item name="Top Bar" type={Box}>
             {leftHeaderText.content}
           </Tree.Item>
-          {leftScrollView.content}
+          {leftScrollView1.content}
+          {leftScrollView2.content}
+          {leftScrollView3.content}
         </Tree.Item>
         <Tree.Item name="Panel Right" type={Box}>
           <Tree.Item name="Top Bar" type={Box}>
             {rightHeaderText.content}
           </Tree.Item>
-          {rightScrollView.content}
+          {rightScrollView1.content}
+          {rightScrollView2.content}
+          {rightScrollView3.content}
         </Tree.Item>
       </Tree.Item>
     ),
