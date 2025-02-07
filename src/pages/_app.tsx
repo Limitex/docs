b2490ce98058a3e0f14829f39bbc1c85793560bf
Limitex <76650151+Limitex@users.noreply.github.com>
2025-02-07 20:19:05 +0900

    feat: add page views to Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import "./globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { NextUIProvider } from "@nextui-org/react";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import { GoogleAnalytics, event } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    event("page_view", { page_path: url });
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
     <>
       <GoogleAnalytics trackPageViews />
